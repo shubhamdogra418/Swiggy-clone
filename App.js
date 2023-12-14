48363f72356b2836af3c44a39bfc22f7fb2aa7b2
shubhamdogra418 <shubhamdogra418@gmail.com>
2023-12-14 19:49:10 +0530

    Day-3 React FC, props

--- App.js
+++ App.js
@@ -1,29 +1,22 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Body from "./Components/Body";
 
-const Heading =(props)=> {
+
+
+
+
+const App =()=> {
     return (
-        <div>
-            <Profile username="shubham.29_" password="shubham123"/>
-            <h1> hello react from {props.fname} { props.lname} </h1>
-            {about}
-        </div>
+        <>
+            <Header/>
+            <Body/>
+            <Footer/>
+        </>
     )
 }
-
-const Profile =(props) => {
-    return (
-        <div> 
-            <h1> Profile is here {props.username} {props.password} </h1>
-            <h1> component composition</h1>
-        </div>
-    )
-}
-const about = (
-    <div>
-        <h1> react element- simple and steady</h1>
-    </div>
-)
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Heading fname="abhsihek" lname="nath"/>)
+root.render(<App/>)
