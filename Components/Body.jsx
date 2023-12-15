2913fe764637436b13378fb8d5d2043cb187d3b2
shubhamdogra418 <shubhamdogra418@gmail.com>
2023-12-15 16:49:03 +0530

    Day-7 API changes

--- Components/Body.jsx
+++ Components/Body.jsx
@@ -1,39 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { RestaurantList } from "../constants/RestaurantList";
-import { IMG_CDN_URL } from "../constants/config";
+import RestaurantCard from "./RestaurantCard";
+import { RestaurantData } from "../constants/RestaurantData";
+import Shimmer from "./Shimmer";
 
 
-
-//config driven UI
-//from  {props] >> {{restaurant}} >> {{name,cuisines, etc..}} directly destructurig in the fly
-const RestaurantCard =({name, cloudinaryImageId, area, costForTwoString, cuisines}) => {
-//destructuring
-    // const {name, cloudinaryImageId, area, costForTwoString, cuisines}= restaurant.data;
-    return (
-        <div className="card">
-            <img src=  {IMG_CDN_URL + cloudinaryImageId}/>
-            <h3> {name} 🍽   </h3> 
-            <h4> {area}</h4>
-            <h4> {cuisines.join(",")} </h4>
-            <h5>{costForTwoString} 🚻</h5>
-            
-        </div>
-    )
-}
 
 
 //not a component but a function to filter the searched data
     function filterData(searchText, restaurants)  {
-       return restaurants.filter((e)=>e.data?.name.toLowerCase()?.includes(searchText.toLowerCase()));
+       return restaurants.filter((e)=>e.info?.name.toLowerCase()?.includes(searchText.toLowerCase()));
     }
+
 
 const Body =()=> {
     //not good way to declare varibales in react
     // const searchText= "Chicken Breast";
+    const[allRestaurants,setAllRestaurants]=useState([]);
+    const[filteredRestaurants,setFilteredRestaurants]=useState(allRestaurants);
     const[searchText, setSearchText]= useState("");
-    const[restaurants,setRestaurants]=useState(RestaurantList);
     console.log(searchText);
-    return (
+
+    useEffect(  ()=> {
+        getRestaurants();
+    },[])
+
+    async function getRestaurants () {
+        const data= await fetch(
+            'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'            );
+        const json= await data.json();
+        console.log(json);
+    //from API
+        setAllRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    //hardcoded data
+        // setAllRestaurants(RestaurantData.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        // setFilteredRestaurants(RestaurantData.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    }
+    
+    if (!allRestaurants) return <h1>No Restaurants Online at this moment</h1>
+    if(filteredRestaurants?.length===0) return <h1> No Searched Restaurants Available</h1>
+    return allRestaurants?.length===0 ? <Shimmer/> : (
         <>
         <div className="search-container">
            <input 
@@ -45,16 +54,19 @@ const Body =()=> {
            <button 
                 className="search-btn"
                 onClick={()=> {
-                    const data= filterData(searchText, restaurants); //parameters to a funcntion
-                    setRestaurants(data);
+                    const data= filterData(searchText, allRestaurants); //parameters to a function
+                    setFilteredRestaurants(data);
                 }}
             > Search </button> 
         </div>
 
-        <div className="resCard">
+        <div className="resCard">   
             {
-                restaurants.map((restaurant)=> {
-                    return <RestaurantCard {...restaurant.data} key={restaurant.data.id}/> })
+                filteredRestaurants.map((restaurant)=> {
+                    return (
+
+                     <RestaurantCard {...restaurant.info} key={restaurant.info.id}/> )}) 
+                    // <RestaurantCard RestaurantList={restaurant}/>)})
             }
         </div>
         </>
--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,3 +1,5 @@
+import React, {useState} from "react";
+
 const Title =()=> {
     return (
         <img className="logo" src="https://www.logopeople.in/wp-content/uploads/2022/10/bahama-eats-N-YC-USA-3.png"/>
@@ -6,6 +8,8 @@ const Title =()=> {
 
 
 const Header = () =>{
+
+    const[isLoggedIn, setIsLoggedIn]=useState(false);
     return (
         <div className="header">
             <Title/>    
@@ -17,6 +21,8 @@ const Header = () =>{
                     <li> Cart </li>
                 </ul>
             </div>
+            {isLoggedIn ? <button onClick={(e)=> setIsLoggedIn(false)}> LogIn</button> :
+            <button onClick={(e)=>setIsLoggedIn(true)}> Logout</button> }
         </div>
     )
 }
